ed793bf0c7f0489a1a76640bebe3329ddbf8fb6a
merinov aleks <merinovaleks1922@gmail.com>
2022-05-23 08:41:02 +0300

    med-roject

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,103 +1,109 @@
-new Swiper('.comment-slider', {
+// --------------------sliders -----------
 
+
+//--------slider doctor--------------------
+
+const sliderDoctors = new Swiper('.doctors-slider', {
    loop: true,
 
-   navigation: false,
+   slidesPerView: 1.5,
+
+   centeredSlides: true,
 
    breakpoints: {
       768: {
-         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-         },
-      },
-   }
-});
-
-new Swiper('.promo-slider', {
-
-   loop: true,
-
-   pagination: false,
-
-   breakpoints: {
-      768: {
-         pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-         } 
+         slidesPerView: 3,
+         spaceBetween: 20,
       }
    },
 
-   thumbs: {
-      swiper: {
-         el: '.mini-slider',
-         slidesPerView: 3,
-         spaceBetween: 16,
-         breakpoints: {
-            768: {
-               direction: 'vertical',
-               spaceBetween: 24,
-            }
-         }
-      }
-   }
+   pagination: {
+      el: '.doctors-slider-pagination',
+      clickable: true,
+   },
+
 });
 
-window.addEventListener('DOMContentLoaded', () => {
-   const menu = document.querySelector('.menu');
-   const hamburger = document.querySelector('.button--nav');
 
-   const promoWrapper = document.querySelector('.promo__wrapper');
+const sliderNews = new Swiper('.news-slider', {
 
-   hamburger.addEventListener('click', () => {
-      menu.classList.toggle('menu--active');
-      promoWrapper.classList.toggle('promo__wrapper--active');
-   });
+   spaceBetween: 20,
 
-
-   const tabWrappers = document.querySelectorAll('.tab-wrapper');
-   if (tabWrappers) {
-
-      tabWrappers.forEach((tabWrapper) => {
-
-         const tabsBtn   = tabWrapper.querySelectorAll(".tabs__tab");
-         const tabsItems = tabWrapper.querySelectorAll('.tabs__slider');
-      
-         tabsBtn.forEach(function(item) {
-            item.addEventListener("click", function() {
-
-               let currentBtn = item;
-               let tabId      = currentBtn.getAttribute("data-tab");
-               let currentTab = document.querySelector(tabId);
-      
-               tabsBtn.forEach(function(item) {
-                  item.classList.remove('tabs__tab--active');
-               });
-      
-               tabsItems.forEach(function(item) {
-                  item.classList.remove('tabs__slider--active');
-               });
-      
-               currentBtn.classList.add('tabs__tab--active');
-               currentTab.classList.add('tabs__slider--active');
-               
-            });
-         });
-
-      });
-   
-   }
+   breakpoints: {
+      768: {
+         slidesPerView: 2,
+         spaceBetween: 20
+      }
+   },
 
 
-   const iconMenu = document.querySelector ('.header-mobile__menu');
-   const mobileMenu = document.querySelector ('.header__wrapper')
 
-   iconMenu.addEventListener('click', () => {
-      iconMenu.classList.toggle('header-mobile__menu--active');
-      mobileMenu.classList.toggle('header__wrapper--active');
-   });
+   slidesPerView: 1,
 
-   
+   pagination: {
+      el: '.news-slider-pagination',
+      clickable: true,
+   },
+
+});
+
+
+const slider = new Swiper('.slider', {
+
+   slidesPerView: 1,
+
+   pagination: {
+      el: '.slide__pagination',
+      clickable: true,
+   },
+
+});
+
+//  ----------------------------- header -------------------------
+
+const body = document.querySelector('body');
+
+const searchBtn = document.querySelector('.header-nav__search');
+const searchInput = document.querySelector('.search');
+
+searchBtn.addEventListener("click", function () {
+   searchInput.classList.toggle('search--active');
+})
+
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.header-nav__main');
+
+burger.addEventListener("click", function () {
+   burger.classList.toggle('burger--active');
+   mobileMenu.classList.toggle('header-nav__main--active');
+   body.classList.toggle('lock');
 
 })
+
+// ------------------------------- tabs ---------------------------
+
+
+const tabsBtn = document.querySelectorAll(".tabs__btn");
+const tabsElements = document.querySelectorAll('.tabs__element');
+
+tabsBtn.forEach(function (item) {
+   item.addEventListener("click", function () {
+
+      let currentBtn = item;
+      let tabId = currentBtn.getAttribute("data-tab");
+      let currentTab = document.querySelector(tabId);
+
+      tabsBtn.forEach(function (item) {
+         item.classList.remove('tabs__btn--active');
+      });
+
+      tabsElements.forEach(function (item) {
+         item.classList.remove('tabs__element--active');
+      });
+
+      currentBtn.classList.add('tabs__btn--active');
+      currentTab.classList.add('tabs__element--active');
+
+   });
+});
+
